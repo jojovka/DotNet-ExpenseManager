727451d53dcb692c7bf0ed590fae297f16c9f308
Kazhi Berik <bkazhi@gmail.com>
2020-11-23 05:55:13 +0600

    First commit

---
+++ ExpenseManager/Models/ExpensesDataAcessLayer.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Здесь будут методы для обработки CRUD операций в базе данных
+namespace ExpenseManager.Models
+{
+    public class ExpensesDataAcessLayer
+    {
+        ExpenseDBContext db = new ExpenseDBContext();
+        public IEnumerable<ExpenseReport> GetAllExpenses()
+        {
+            try
+            {
+                return db.ExpenseReport.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Фильтровать запись в поиске   
+        public IEnumerable<ExpenseReport> GetSearchResult(string searchString)
+        {
+            List<ExpenseReport> exp = new List<ExpenseReport>();
+            try
+            {
+                exp = GetAllExpenses().ToList();
+                return exp.Where(x => x.ItemName.IndexOf(searchString, StringComparison.OrdinalIgnoreCase) != -1);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Добавить новый пункт о расходе         
+        public void AddExpense(ExpenseReport expense)
+        {
+            try
+            {
+                db.ExpenseReport.Add(expense);
+                db.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Обновить данные о расходе    
+        public int UpdateExpense(ExpenseReport expense)
+        {
+            try
+            {
+                db.Entry(expense).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Данные по конкретным расходам    
+        public ExpenseReport GetExpenseData(int id)
+        {
+            try
+            {
+                ExpenseReport expense = db.ExpenseReport.Find(id);
+                return expense;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Удалить пункт о расходе    
+        public void DeleteExpense(int id)
+        {
+            try
+            {
+                ExpenseReport emp = db.ExpenseReport.Find(id);
+                db.ExpenseReport.Remove(emp);
+                db.SaveChanges();
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        // Расходы за последние пол года  
+        public Dictionary<string, decimal> CalculateMonthlyExpense()
+        {
+            ExpensesDataAcessLayer objexpense = new ExpensesDataAcessLayer();
+            List<ExpenseReport> lstEmployee = new List<ExpenseReport>();
+
+            Dictionary<string, decimal> dictMonthlySum = new Dictionary<string, decimal>();
+
+            decimal foodSum = db.ExpenseReport.Where
+                (cat => cat.Category == "Food" && (cat.ExpenseDate > DateTime.Now.AddMonths(-7)))
+                .Select(cat => cat.Amount)
+                .Sum();
+
+            decimal shoppingSum = db.ExpenseReport.Where
+               (cat => cat.Category == "Shopping" && (cat.ExpenseDate > DateTime.Now.AddMonths(-7)))
+               .Select(cat => cat.Amount)
+               .Sum();
+
+            decimal travelSum = db.ExpenseReport.Where
+               (cat => cat.Category == "Travel" && (cat.ExpenseDate > DateTime.Now.AddMonths(-7)))
+               .Select(cat => cat.Amount)
+               .Sum();
+
+            decimal healthSum = db.ExpenseReport.Where
+               (cat => cat.Category == "Health" && (cat.ExpenseDate > DateTime.Now.AddMonths(-7)))
+               .Select(cat => cat.Amount)
+               .Sum();
+
+            dictMonthlySum.Add("Food", foodSum);
+            dictMonthlySum.Add("Shopping", shoppingSum);
+            dictMonthlySum.Add("Travel", travelSum);
+            dictMonthlySum.Add("Health", healthSum);
+
+            return dictMonthlySum;
+        }
+
+        // Расходы за последние четыре недели  
+        public Dictionary<string, decimal> CalculateWeeklyExpense()
+        {
+            ExpensesDataAcessLayer objexpense = new ExpensesDataAcessLayer();
+            List<ExpenseReport> lstEmployee = new List<ExpenseReport>();
+
+            Dictionary<string, decimal> dictWeeklySum = new Dictionary<string, decimal>();
+
+            decimal foodSum = db.ExpenseReport.Where
+                (cat => cat.Category == "Food" && (cat.ExpenseDate > DateTime.Now.AddDays(-7)))
+                .Select(cat => cat.Amount)
+                .Sum();
+
+            decimal shoppingSum = db.ExpenseReport.Where
+               (cat => cat.Category == "Shopping" && (cat.ExpenseDate > DateTime.Now.AddDays(-28)))
+               .Select(cat => cat.Amount)
+               .Sum();
+
+            decimal travelSum = db.ExpenseReport.Where
+               (cat => cat.Category == "Travel" && (cat.ExpenseDate > DateTime.Now.AddDays(-28)))
+               .Select(cat => cat.Amount)
+               .Sum();
+
+            decimal healthSum = db.ExpenseReport.Where
+               (cat => cat.Category == "Health" && (cat.ExpenseDate > DateTime.Now.AddDays(-28)))
+               .Select(cat => cat.Amount)
+               .Sum();
+
+            dictWeeklySum.Add("Food", foodSum);
+            dictWeeklySum.Add("Shopping", shoppingSum);
+            dictWeeklySum.Add("Travel", travelSum);
+            dictWeeklySum.Add("Health", healthSum);
+
+            return dictWeeklySum;
+        }
+    }
+}
